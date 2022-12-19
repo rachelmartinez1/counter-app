5edbdcfb2ce7ecb2d907c11a59d90108f5fc3879
Rachel Martinez <rachelmartinez101@gmail.com>
2022-12-19 14:09:35 -0700

    Add actions to parent class, create a single source of truth for the counter value state, add a navbar

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,23 +2,29 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 0},
-            { id: 2, value: 0},
-            { id: 3, value: 0},
-            { id: 4, value: 0},
-        ]
-    };
 
     render() {
-        return(
+        const { onReset, onDelete, counters, onIncrement } = this.props;
+
+        return (
             <div>
-                { this.state.counters.map(counter => 
-                <Counter key={counter.id} value={counter.value} />)}
+                <button 
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2"
+                    >
+                        Reset
+                </button>
+                {counters.map(counter => (
+                    <Counter 
+                        key={counter.id} 
+                        onDelete={onDelete} 
+                        onIncrement={onIncrement}
+                        counter={counter}
+                    />
+                ))}
             </div>
         );
-    }
+    };
 }
 
 export default Counters;
